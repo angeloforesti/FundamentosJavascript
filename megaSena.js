6714248af6685e3855cc3807c0a52936041fb4a9
angelo foresti neto <angeloforestin@gmail.com>
2023-01-12 11:01:47 -0300

    Create megaSena.js

---
+++ megaSena.js
@@ -0,0 +1,40 @@
+//usando JS para fazer o seu cartao na megasena
+
+function rand({min = 1, max = 60}){
+                 
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+const obj1={ }
+console.log(rand(obj1))
+
+const obj2={ }
+console.log(rand(obj2))
+
+const obj3={ }
+console.log(rand(obj3))
+
+const obj4={ }
+console.log(rand(obj4))
+
+const obj5={ }
+console.log(rand(obj5))
+
+const obj6={ }
+console.log(rand(obj6))
+
+// ou 
+
+console.log(rand({}))
+
+console.log(rand({}))
+
+console.log(rand({}))
+
+console.log(rand({}))
+
+console.log(rand({}))
+
+console.log(rand({}))
+
+

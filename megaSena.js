2725054aff2b3e7f223044a6a2fa3120d9411fec
angelo foresti neto <angeloforestin@gmail.com>
2023-01-12 19:56:04 -0300

    Update megaSena.js

--- megaSena.js
+++ megaSena.js
@@ -1,40 +1,17 @@
-//usando JS para fazer o seu cartao na megasena
-
 function rand({min = 1, max = 60}){
+
                  
-    const valor = Math.random() * (max - min) + min
-    return Math.floor(valor)
-}
-const obj1={ }
-console.log(rand(obj1))
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor);
+    };
 
-const obj2={ }
-console.log(rand(obj2))
+    const numbers = [];
 
-const obj3={ }
-console.log(rand(obj3))
+do {
+  const randomNumber = rand({ min: 1, max: 60 });
+  if (!numbers.includes(randomNumber)) numbers.push(randomNumber);
+} while (numbers.length < 6);
+console.log(numbers.sort());
 
-const obj4={ }
-console.log(rand(obj4))
-
-const obj5={ }
-console.log(rand(obj5))
-
-const obj6={ }
-console.log(rand(obj6))
-
-// ou 
-
-console.log(rand({}))
-
-console.log(rand({}))
-
-console.log(rand({}))
-
-console.log(rand({}))
-
-console.log(rand({}))
-
-console.log(rand({}))
-
-
+    
+    

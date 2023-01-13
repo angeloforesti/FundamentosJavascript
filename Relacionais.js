a2f8bfa6c91ec662ee6738bf2dae4d6c7b9fc1d0
angelo foresti neto <angeloforestin@gmail.com>
2023-01-13 11:00:24 -0300

    Delete Relacionais.js

--- Relacionais.js
+++
@@ -1,9 +0,0 @@
-console.log('01)', '1' == 1) //leva em consideração apenas o valor
-console.log('02)', '1' === 1)//leva em consideração o tipo e o valor
-console. log('03)', '3' != 3)
-console. log('04)', '3' !== 3)
-
-const d1 = new Date(0)
-const d2 = new Date(0)
-console. log('09)', d1 === d2)
-console. log('10)', d1 == d2)

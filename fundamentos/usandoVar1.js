558b2f92097b5b6cf44d8b8777724067c546ff29
angelo foresti neto <angeloforestin@gmail.com>
2023-01-13 11:00:51 -0300

    Create usandoVar1.js

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+            }
+        }
+    }
+}
+console.log(sera) //variavel var mesmo se estiver em um bloco de codigo estara visivel para todos (menos se for função)
+
+function teste (){
+    var local = 123
+    console.log(local)
+}
+
+//teste()
+//console.log(local)
+
+// quando se cria variavel que vai para o escopo global a grande desvantagem que é facil sobrescrever o valor

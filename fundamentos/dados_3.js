968b0f9a5b1eac9fa6d7334f355f309b3618492c
angelo foresti neto <angeloforestin@gmail.com>
2023-01-13 11:01:32 -0300

    Create dados_3.js

---
+++ fundamentos/dados_3.js
@@ -0,0 +1,15 @@
+let idade=31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario=4578. 
+console.log(typeof salario);
+
+let estachovendo=true // ou false
+console.log(typeof estachovendo);
+
+console.log(typeof"Teste");
+console.log(typeof'Teste');
+
+
+// 3 tipos de variaveis basicas suportados por JS!

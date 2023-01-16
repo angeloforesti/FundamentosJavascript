f58ccb54fcd54eb19317abc2c92ee32a2f51f2d7
angelo foresti neto <angeloforestin@gmail.com>
2023-01-16 09:18:17 -0300

    Create ifElse.js

---
+++ controle/ifElse.js
@@ -0,0 +1,11 @@
+const resultado = function(nota) {
+    if(nota >= 7){
+        console.log('aprovado')
+    }else{
+        console.log('reprovado')
+    }
+}    
+        
+resultado(6)    
+resultado(7)    
+resultado(8)

82d9ed3169d22365f74b1ead210787ff5c7ab2f2
angelo foresti neto <angeloforestin@gmail.com>
2023-01-16 10:30:29 -0300

    Create switch.js

---
+++ controle/switch.js
@@ -0,0 +1,31 @@
+const imprimirResultado = function (nota) {
+    switch (Math.floor(nota)) {
+        case 10:
+        case 9:
+            console.log('Quadro de Honra')
+            break
+        case 8:
+        case 7:
+            console.log('Aprovado')
+             break
+        case 6: case 5: case 4:
+            console.log('recuperação')   
+            break
+        case 3: case 2: case 1: case 0:
+            console.log('reprovado') 
+            break
+        default:
+            console.log('nota invalida')         
+    }
+ 
+}
+
+imprimirResultado(7)
+imprimirResultado(10)
+imprimirResultado(6,9)
+imprimirResultado(5)
+imprimirResultado(1)
+imprimirResultado(0)
+imprimirResultado(NaN)
+imprimirResultado(-1)
+imprimirResultado(11)

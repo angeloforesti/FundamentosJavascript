8914a9ec5c6ddcb9c6668dbb3452bed9ea89517f
angelo foresti neto <angeloforestin@gmail.com>
2023-01-16 09:05:18 -0300

    Update if1.js

---
+++ controle/if1.js
@@ -0,0 +1,30 @@
+function soBoaNoticia(nota) {
+    if(nota >= 7){
+        console.log('aprovado com ' + nota)
+    }
+
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+
+
+function verdade(valor){
+    if(valor){
+        console.log('é verdade... ' + valor)
+    }
+}
+
+verdade()
+verdade(null)
+verdade(undefined)
+verdade(NaN)
+verdade('')
+verdade(0)
+verdade(-1)
+verdade(' ')
+verdade('?')
+verdade([])
+verdade([1,2])
+verdade({})

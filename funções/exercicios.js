605b2d68e9416143e20cbc3ca3fe92aa04c7abd8
angeloforesti <angeloforestin@gmail.com>
2023-02-01 19:34:49 -0300

    Update exercicios.js

--- funções/exercicios.js
+++ funções/exercicios.js
@@ -1,13 +1,19 @@
 /*01) Crie uma função que dado dois valores (passados como parâmetros)
 mostre no console a soma, subtração, multiplicação e divisão desses valores.*/
 const calc = (n1, n2) => {
-	console.log(`Resultados
-	Soma: ${n1} + ${n2} =  ${n1 + n2}
-	Subtração: ${n1} - ${n2} =  ${n1 - n2}
-	Multiplicação: ${n1} * ${n2} =  ${n1 * n2}
-	Divisão: ${n1} / ${n2} =  ${n1 / n2}`)
+    let soma = n1 + n2;
+    console.log(soma)
+    let mult = n1 * n2;
+    console.log(mult)
+    let div = n1 / n2;
+    console.log(div)
+    let dim = n1 - n2;
+    console.log(dim)
+    
 }
-calc(2, 5)
+
+calc(2,3)
+	
 
 
 /*02) Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:

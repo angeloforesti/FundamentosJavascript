0a14b733b835dba29767f166b3439e012bc44a2e
angeloforesti <angeloforestin@gmail.com>
2023-02-01 09:49:22 -0300

    Update callback3.js

--- funções/callback3.js
+++ funções/callback3.js
@@ -1,5 +1,7 @@
 //exemplo de callback no browser
 
-document.getElementsByTagName('body'[0].onclick = function (e){
+document.getElementsByTagName('body')[0].onclick = function (e){
     console.log('o evento ocorreu')
-})
+}
+
+document.getElementsByTagName('body')[0].onclick = _ =>console.log('o evento ocorreu com uma arrow function')

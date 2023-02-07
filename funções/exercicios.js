e5e31e4bf4f8231368ad6c730fe97794db07d870
angeloforesti <angeloforestin@gmail.com>
2023-02-07 12:42:50 -0300

    Update exercicios.js

--- funções/exercicios.js
+++ funções/exercicios.js
@@ -243,4 +243,56 @@ util(5)
 util(7)
 util(8)
 
+/*14) Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três
+casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
+escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
+também um default, que retornará uma mensagem de erro no console.*/
+
+const venderFruta= (fruta) => {
+    switch (fruta) {
+        case 'maçã':
+            console.log('Não vendemos esta fruta aqui.')
+            break
+        case 'kiwi':
+            console.log('Estamos com escassez de kiwi.')
+            break
+        case 'melancia':
+            console.log('Aqui está, custa R$ 3,00 o quilo.')
+            break
+        default:
+            console.log('Erro, fruta inválida.')
+    }
+}
+
+venderFruta('maçã')
+venderFruta('kiwi')
+venderFruta('melancia')
+venderFruta('alcatra')
+
+/*15) Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda
+possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o
+comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza
+que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console:
+“Não trabalhamos com este tipo de automóvel aqui”.*/
+
+const carros= (carro) => {
+    switch (carro) {
+        case 'hatch':
+            console.log('Compra efetuada com sucesso aqui.')
+            break
+        case 'camionete':
+            console.log('Tem certeza que não prefere este modelo?')
+            break
+        case 'sedan':
+            console.log('Tem certeza que não prefere este modelo?')
+            break
+        default:
+            console.log('indisponivel')
+    }
+}
+
+carros('hatch')
+carros('camionete')
+carros('sedan')
+carros('moto')
 

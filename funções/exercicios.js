7df7d7af3dba1fca3251ddd23697c5d558bb79e9
angeloforesti <angeloforestin@gmail.com>
2023-02-08 09:48:16 -0300

    Update exercicios.js

--- funções/exercicios.js
+++ funções/exercicios.js
@@ -322,3 +322,29 @@ console.log(calculadora(2, '*', 3));
 console.log(calculadora(2, '/', 3));
 console.log(calculadora(2, 'a', 3));
 
+/*17) Um funcionário irá receber um aumento de acordo com o seu plano de
+trabalho, de acordo com a tabela abaixo:
+Plano Aumento
+A 10%
+B 15%
+C 20%
+Faça uma função que leia o plano de trabalho e o salário atual de um funcionário e calcula e imprime o seu
+novo salário. Use a estrutura switch e faça um caso default que indique que o plano é inválido*/
+
+function aumento (salario, aumentoReal,) {
+    switch (aumentoReal) {
+        case 'A':
+            return salario + salario*0.10
+        case 'B':
+            return salario + salario*0.15
+        case 'C':
+            return salario + salario*0.20
+        default:
+            return 'Operação inválida'
+    }
+}
+
+console.log(aumento(1000, 'A'));
+console.log(aumento(1000, 'B'));
+console.log(aumento(1000, 'C'));
+console.log(aumento(1000, 'D'));

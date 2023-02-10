940e1df9d2ddfe5c8b2e734f1a01b2f3bd88bace
angeloforesti <angeloforestin@gmail.com>
2023-02-10 16:44:06 -0300

    Create oo.js

---
+++ orientaçãoObjetos/oo.js
@@ -0,0 +1,20 @@
+// Código não executável
+// Procedural -> baseado no procedimento, pode reutilizar linha de codigos sem go2
+processamento(valor1,valor2,valor3)
+/* OO - Orientação à Objeto, antigamente se tinha foco em funções que manipulavam dados
+ com OO voce tem dados e dentro desses dados voce tem funcoes*/
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+    processamento(){
+      // ...
+    }
+}
+objeto.processamento() // foco passou a ser o objeto, antes era a função. ( no caso de orientação à objeto)
+
+// --------------------Principios Importantes ----------------------------------------------
+// 1. Abstração, é transformar as coisas de uma maneira simplificada em um objeto.
+// 2. Encapsulamento, detalhes da implementação escondidos e representa-los de maneira simples para interface
+// 3. Herança (prototype)
+// 4. Polimorfismo

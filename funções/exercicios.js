03fb0cf4b8e577c8ad144de70698d92d684e7b67
angeloforesti <angeloforestin@gmail.com>
2023-02-10 15:08:45 -0300

    Update exercicios.js

--- funções/exercicios.js
+++ funções/exercicios.js
@@ -606,8 +606,51 @@ function calcularTempo(alturaMenor, taxaAlturaMenor, alturaMaior, taxaAlturaMaio
 
 console.log(calcularCrescimento(150, 2, 130, 4));
 
+/*) Ler um vetor de números inteiros e imprimir quantos são pares e quantos são ímpares.*/
 
 
+const leitorDeArray = numeros => {
+    let contadorPar = 0
+    let contadorImpar = 0 
+
+    for (let i = 0; i < numeros.length; i++){
+        if (numeros[i] % 2 === 0 ) {
+            contadorPar++
+        }else{
+            contadorImpar++
+        }
+    }
+    console.log("a quantia de pares é de " +contadorPar)
+console.log("a quantia de impares é de " +contadorImpar)
+
+}
+
+
+leitorDeArray([1,2,3,4,5,6,7,8])
+
+
+/*29) Utilizando a estrutura de repetição for, faça uma função que percorra um vetor e conte quantos números
+deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos
+deles estão fora do intervalo, escrevendo estas informações.*/
+
+const percorrendoArray = numeros => {
+    let contadorIntervalo = 0
+    let contadorFora = 0
+
+    for (let i = 0; i < numeros.length; i++){
+        if (numeros[i] >= 10 && numeros[i] <= 20   ) {
+            contadorIntervalo++
+        }else{
+            contadorFora++
+        }
+    }
+    console.log("a quantia de numeros entre 10 e 20 é de " +contadorIntervalo)
+console.log("a quantia restante é de " +contadorFora)
+
+}
+
+
+percorrendoArray([10,20,30,40,50,60,70,88])
 
 
 

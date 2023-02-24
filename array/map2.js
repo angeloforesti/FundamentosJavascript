c77b9af1804b50818ed3f1436f2da61cf04fbe9b
angeloforesti <angeloforestin@gmail.com>
2023-02-24 00:12:47 -0300

    Update map2.js

--- array/map2.js
+++ array/map2.js
@@ -5,7 +5,7 @@ const carrinho = [
     '{"Nome":"Caneta", "preco": 7.50}'
   ]
   // Desafio Retornar array apenas com os preços
-  const paraObjeto = json => JSON.parse(json) // criado para traduzir o JSON em Array
+  const paraObjeto = json => JSON.parse(json) // criado para transformar  o JSON em Array
   const apenasPreco = produto => produto.preco // Criado para filtrar o preco 
   resultado = carrinho.map(paraObjeto).map(apenasPreco) // traduziu carrinho e filtrou apenas o preco
   console.log(resultado)

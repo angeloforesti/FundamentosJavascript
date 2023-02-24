9be4dafadee15fe726c7a0a44291d28dfb11be80
angeloforesti <angeloforestin@gmail.com>
2023-02-23 23:53:06 -0300

    Update forEach2.js

--- array/forEach2.js
+++ array/forEach2.js
@@ -1,4 +1,7 @@
-Array.prototype.forEach2 = function(callback){ // functoin para simular o foreach -- callback poderia ser qualquer nome
+Array.prototype.forEach2 = function(callback){ /* functoin para simular o foreach -- callback poderia ser qualquer nome 
+acessamos o prototype do array (que é um obj) atraves do .prototype e nele declaramos a funcao forEach2
+*/
+
     for(let i=0; i<this.length; i++){ // this aponta para o array
       callback(this[i], i, this) // this aponta para o array completo
     }

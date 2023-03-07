15ad997a4d89fa6f0e428dfefbc6a2abba9d323a
angeloforesti <angeloforestin@gmail.com>
2023-03-07 16:57:15 -0300

    Update exercicios.js

--- exercicios/exercicios.js
+++ exercicios/exercicios.js
@@ -806,4 +806,45 @@ const inicioFim = (inicio, fim) => {
   }
   
   console.log(inicioFim(1, 100))
-  
+
+/*39) Crie uma função que receba dois vetores de igual tamanho e troque seus elementos de modo que o
+primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
+Faça a troca sem utilizar uma variável auxiliar.*/
+
+const trocaTroca = (array1, array2) => {
+    for(let i = 0; i >= array1.length; i++){
+        const x = array1[i]
+        array1.splice(i, 1, array2[i])
+        array2.splice[i, 1, x]
+       
+    
+    }
+console.log([array1])
+console.log([array2])
+
+
+}
+trocaTroca([10, 20, 30], [3, 2, 1])
+
+/*40) Faça uma função que receba como parâmetro um vetor de notas e mostre os conceitos de cada uma de
+modo que de 0,0 a 4,9 seja atribuído o conceito D, de 5,0 a 6,9 seja atribuído o conceito C, de 7,0 a 8,9 o
+conceito B e de 9,0 a 10,0 o conceito A.*/
+
+const avaliadorNotas = notas => {
+    for(let i = 0; i < notas.length; i++) {
+      if (notas[i] >= 0 && notas[i] <= 4.9) {
+        console.log(`Nota ${notas[i]} - Conceito D`);
+      } else if (notas[i] >= 5 && notas[i] <= 6.9) {
+        console.log(`Nota ${notas[i]} - Conceito C`);
+      } else if (notas[i] >= 7 && notas[i] <= 8.9) {
+        console.log(`Nota ${notas[i]} - Conceito B`);
+      } else if (notas[i] >= 9 && notas[i] <= 10) {
+        console.log(`Nota ${notas[i]} - Conceito A`);
+      } else {
+        console.log(`Nota ${notas[i]} - Valor inválido`);
+      }
+    }
+  }
+
+
+avaliadorNotas([5,8,9])
